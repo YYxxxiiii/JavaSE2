39bb294a8d68f145dbde090c0148e0500639b092
YYxxxiiii <axii_@outlook.com>
2020-08-21 20:52:02 +0800

    BinaryTree

---
+++ InterviewTree/BinaryTree.java
@@ -0,0 +1,74 @@
+package InterviewTree;
+
+//二叉树的递归遍历
+class Node {
+    public char val;
+    public Node left;
+    public Node right;
+
+    public Node(char val) {
+        this.val = val;
+    }
+}
+public class BinaryTree {
+    //手动构造一棵树
+    public static Node build() {
+        Node a = new Node('A');
+        Node b = new Node('B');
+        Node c = new Node('C');
+        Node d = new Node('D');
+        Node e = new Node('E');
+        Node f = new Node('F');
+        Node g = new Node('G');
+        Node h = new Node('H');
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        e.left = g;
+        g.right = h;
+        c.right = f;
+
+        return a;
+    }
+
+    //前序遍历
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    //中序遍历
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+
+    //后序遍历
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = build();
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
+    }
+}
