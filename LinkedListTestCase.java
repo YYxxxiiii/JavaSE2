978a8f601bfb7c02c9e4c495a0a61cacbe2938ae
YYxxxiiii <axii_@outlook.com>
2020-09-19 21:16:45 +0800

    a

---
+++ LinkedListTestCase.java
@@ -0,0 +1,11 @@
+package linkedlist;
+
+public class LinkedListTestCase {
+    public static void main(String[] args) {
+        List list = new LinkedList();
+        list.add(31);
+        list.add(32);
+        list.add(33);
+        System.out.println(list);   // [31, 32, 33]
+    }
+}
